e72e48e629029999f53796e5842cb715dfc3c3e5
Josiah <63004866+Necloremius@users.noreply.github.com>
2022-08-02 20:47:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,28 @@ import numpy as np
 import pandas as pd
 import sklearn
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_digits
+from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
 
+iris = load_iris()
 
-digits = load_digits()
+df = pd.DataFrame(iris.data)
+df['target'] = iris.target
 
-df = pd.DataFrame(digits.data)
-df['target'] = digits.target
-df.head()
-
-x_train, x_test, y_train, y_test = train_test_split(df.drop(['target'], axis='columns'), digits.target, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(df.drop(['target'], axis='columns'), iris.target, test_size=0.2)
 
 model = RandomForestClassifier()
 model.fit(x_train, y_train)
 
-model.score(x_test, y_test)
+y_predicted = model.predict(x_test)
+sns.set_theme(style="whitegrid")
+
+cm = confusion_matrix(y_test,y_predicted)
+
+plt.figure(figsize=(10,7))
+sns.heatmap(cm, annot=True)
+plt.xlabel('Predicted')
+plt.ylabel('Truth')
